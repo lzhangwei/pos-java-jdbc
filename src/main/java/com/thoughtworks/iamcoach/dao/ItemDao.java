001890f863ced94544b5677da4ddf944ebc2711b
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 15:10:12 +0800

    delete useless code

--- src/main/java/com/thoughtworks/iamcoach/dao/ItemDao.java
+++ src/main/java/com/thoughtworks/iamcoach/dao/ItemDao.java
@@ -4,9 +4,6 @@ import com.thoughtworks.iamcoach.vo.Item;
 
 import java.util.ArrayList;
 
-/**
- * Created by zhangwei on 14-10-26.
- */
 public interface ItemDao {
     ArrayList<Item> getItems();
 
