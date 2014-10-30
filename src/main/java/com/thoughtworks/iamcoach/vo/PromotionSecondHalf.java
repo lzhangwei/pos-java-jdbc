c3e7b7f027e5a2d7ce727b2711d28561a0594ff9
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 14:42:52 +0800

    update caculate promotion price

--- src/main/java/com/thoughtworks/iamcoach/vo/PromotionSecondHalf.java
+++ src/main/java/com/thoughtworks/iamcoach/vo/PromotionSecondHalf.java
@@ -1,8 +1,8 @@
 package com.thoughtworks.iamcoach.vo;
 
-public class PromotionSecondHalf extends Promotion{
+public class PromotionSecondHalf extends Promotion {
     @Override
     public double caculatePromotionPrice(Item item, double num) {
-        return 0;
+        return num / 2 * (item.getPrice() / 2.0);
     }
 }
