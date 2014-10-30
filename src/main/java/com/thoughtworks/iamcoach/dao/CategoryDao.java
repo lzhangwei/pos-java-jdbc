91597c5ea8d46b381920e2be91687093ad75eb2a
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 15:36:53 +0800

    add getCategoryById function

--- src/main/java/com/thoughtworks/iamcoach/dao/CategoryDao.java
+++ src/main/java/com/thoughtworks/iamcoach/dao/CategoryDao.java
@@ -1,4 +1,7 @@
 package com.thoughtworks.iamcoach.dao;
 
+import com.thoughtworks.iamcoach.vo.Category;
+
 public interface CategoryDao {
+    public Category getCategoryById(int id);
 }
--- src/main/java/com/thoughtworks/iamcoach/dao/CategoryDaoImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/dao/CategoryDaoImpl.java
@@ -1,4 +1,46 @@
 package com.thoughtworks.iamcoach.dao;
 
-public class CategoryDaoImpl {
+import com.thoughtworks.iamcoach.util.DBUtil;
+import com.thoughtworks.iamcoach.vo.Category;
+import com.thoughtworks.iamcoach.vo.Promotion;
+import com.thoughtworks.iamcoach.vo.PromotionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CategoryDaoImpl implements CategoryDao{
+
+    private DBUtil dbUtil = new DBUtil();
+
+    @Override
+    public Category getCategoryById(int id) {
+        Category category = null;
+        Connection conn = dbUtil.getConnection();
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        String sql = "SELECT * FROM categories WHERE id=?";
+
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            rs = pstmt.executeQuery(sql);
+            rs.next();
+            category.setId(rs.getInt("id"));
+            category.setName(rs.getString("name"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                dbUtil.close();
+                pstmt.close();
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return category;
+    }
 }
