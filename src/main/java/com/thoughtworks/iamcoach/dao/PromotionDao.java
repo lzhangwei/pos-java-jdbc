8a88463759b50d900f0983718ddd4ebd9648264f
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 14:56:15 +0800

    add functions

--- src/main/java/com/thoughtworks/iamcoach/dao/PromotionDao.java
+++ src/main/java/com/thoughtworks/iamcoach/dao/PromotionDao.java
@@ -1,4 +1,10 @@
 package com.thoughtworks.iamcoach.dao;
 
+import com.thoughtworks.iamcoach.vo.Promotion;
+
+import java.util.List;
+
 public interface PromotionDao {
+    public Promotion getPromotionById(int id);
+    public List<Promotion> getPromotions();
 }
