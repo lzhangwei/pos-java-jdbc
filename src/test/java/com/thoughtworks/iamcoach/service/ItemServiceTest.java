103f4e765b5e53a2b28f9e766e6b3b16ebfaead1
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 09:43:10 +0800

    add createItems function

--- src/test/java/com/thoughtworks/iamcoach/service/ItemServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/service/ItemServiceTest.java
@@ -1,4 +1,26 @@
 package com.thoughtworks.iamcoach.service;
 
+import com.thoughtworks.iamcoach.vo.Item;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemServiceTest {
+
+    private List<Item> createItems() {
+        List<Item> result = new ArrayList<Item>();
+        result.add(new Item(0, "ITEM000000", "可口可乐", "瓶", 3.00));
+        result.add(new Item(1, "ITEM000001", "雪碧", "瓶", 3.00));
+        result.add(new Item(2, "ITEM000002", "苹果", "瓶", 3.00));
+        result.add(new Item(3, "ITEM000003", "荔枝", "瓶", 3.00));
+        result.add(new Item(4, "ITEM000004", "电池", "瓶", 3.00));
+        result.add(new Item(5, "ITEM000005", "方便面", "瓶", 3.00));
+        return result;
+    }
+
+    @Test
+    public void should_be_mocked() {
+
+    }
 }
