edfc1c5d8f4396798c553a3265352023251c7114
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 15:07:22 +0800

    update getPromotionById function

--- src/main/java/com/thoughtworks/iamcoach/dao/PromotionDaoImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/dao/PromotionDaoImpl.java
@@ -1,13 +1,45 @@
 package com.thoughtworks.iamcoach.dao;
 
+import com.thoughtworks.iamcoach.util.DBUtil;
 import com.thoughtworks.iamcoach.vo.Promotion;
+import com.thoughtworks.iamcoach.vo.PromotionFactory;
 
+import java.sql.*;
 import java.util.List;
 
 public class PromotionDaoImpl implements PromotionDao{
+    private DBUtil dbUtil = new DBUtil();
     @Override
     public Promotion getPromotionById(int id) {
-        return null;
+        Promotion promotion = null;
+        Connection conn = dbUtil.getConnection();
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        String sql = "SELECT * FROM promotions WHERE id=?";
+
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            rs = pstmt.executeQuery(sql);
+            rs.next();
+            int type = rs.getInt("type");
+            promotion = PromotionFactory.getPromotionByType(type);
+            promotion.setId(rs.getInt("id"));
+            promotion.setPromotionDesc(rs.getString("prodesc"));
+            promotion.setType(rs.getInt("type"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                dbUtil.close();
+                pstmt.close();
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return promotion;
     }
 
     @Override
