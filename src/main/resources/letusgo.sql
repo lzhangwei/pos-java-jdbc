309b5334804f3e7ec61a4f382f8e8ce209d17b06
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 22:28:04 +0800

    update sql

--- src/main/resources/letusgo.sql
+++ src/main/resources/letusgo.sql
@@ -15,15 +15,8 @@ CREATE TABLE items(
   name varchar(20),
   unit varchar(8),
   price double,
-  category int foreign key references categories(id)
-);
-
-DROP TABLE IF EXISTS promotions;
-
-CREATE TABLE promotions(
-  id int auto_increment primary key,
-  prodesc varchar(50),
-  type int
+  category int,
+  FOREIGN KEY(category) REFERENCES categories(id)
 );
 
 DROP TABLE IF EXISTS promotions;
@@ -37,8 +30,10 @@ CREATE TABLE promotions(
 DROP TABLE IF EXISTS items_promotions;
 
 CREATE TABLE items_promotions(
-  itemId int not null foreign key REFERENCES items(id),
-  promotionId int not null foreign key REFERENCES promotions(id),
+  itemId int not null,
+  FOREIGN KEY(itemId)REFERENCES items(id),
+  promotionId int not null,
+  FOREIGN KEY(promotionId) REFERENCES promotions(id),
   discount double,
   primary key(itemId, promotionId)
 );
@@ -48,12 +43,12 @@ insert into categories values (null,'水果');
 insert into categories values (null,'食品');
 insert into categories values (null,'生活用品');
 
-insert into items values(null,'ITEM000000','可口可乐','瓶',3.0);
-insert into items values(null,'ITEM000001','雪碧','瓶',3.0);
-insert into items values(null,'ITEM000002','苹果','千克',8.0);
-insert into items values(null,'ITEM000003','荔枝','千克',12.0);
-insert into items values(null,'ITEM000004','方便面','袋',2.5);
-insert into items values(null,'ITEM000005','电池','节',2.0);
+insert into items values(null,'ITEM000000','可口可乐','瓶',3.0,1);
+insert into items values(null,'ITEM000001','雪碧','瓶',3.0,1);
+insert into items values(null,'ITEM000002','苹果','千克',8.0,2);
+insert into items values(null,'ITEM000003','荔枝','千克',12.0,2);
+insert into items values(null,'ITEM000004','方便面','袋',2.5,3);
+insert into items values(null,'ITEM000005','电池','节',2.0,4);
 
 insert into promotions values(null,'buy_two_get_one_free_promotion',1);
 insert into promotions values(null,'second_half_price_promotion',2);
