d76b14b1beef570b25f78ac45878218eef334c9a
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 13:46:28 +0800

    add fielsd

--- src/main/java/com/thoughtworks/iamcoach/vo/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/vo/Promotion.java
@@ -1,4 +1,40 @@
 package com.thoughtworks.iamcoach.vo;
 
 public class Promotion {
+    private int  id;
+    private String promotionDesc;
+    private int type;
+
+    public Promotion() {
+    }
+
+    public Promotion(int id, String promotionDesc, int type) {
+        this.id = id;
+        this.promotionDesc = promotionDesc;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPromotionDesc() {
+        return promotionDesc;
+    }
+
+    public void setPromotionDesc(String promotionDesc) {
+        this.promotionDesc = promotionDesc;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
 }
