1455d79d87b5cfc7891ad936eb01f63d56856145
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 14:44:29 +0800

    update caculate promotion price

--- src/main/java/com/thoughtworks/iamcoach/vo/PromotionTwoForOne.java
+++ src/main/java/com/thoughtworks/iamcoach/vo/PromotionTwoForOne.java
@@ -1,8 +1,8 @@
 package com.thoughtworks.iamcoach.vo;
 
-public class PromotionTwoForOne extends Promotion{
+public class PromotionTwoForOne extends Promotion {
     @Override
     public double caculatePromotionPrice(Item item, double num) {
-        return 0;
+        return num / 3 * item.getPrice();
     }
 }
