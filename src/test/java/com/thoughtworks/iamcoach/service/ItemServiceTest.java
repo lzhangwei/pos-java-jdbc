5864a53471387f197c2746f1235e46b8fe0971b5
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 13:42:57 +0800

    update test

--- src/test/java/com/thoughtworks/iamcoach/service/ItemServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/service/ItemServiceTest.java
@@ -1,10 +1,11 @@
 package com.thoughtworks.iamcoach.service;
 
 import com.thoughtworks.iamcoach.dao.ItemDaoImpl;
-import com.thoughtworks.iamcoach.vo.Item;
+import com.thoughtworks.iamcoach.vo.*;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -13,13 +14,17 @@ import static org.mockito.Mockito.when;
 public class ItemServiceTest {
 
     private ArrayList<Item> createItems() {
+        Category category = new Category(1,"饮料");
+        List<Promotion> promotionList = new ArrayList<Promotion>();
+        promotionList.add(new PromotionDiscount());
+        promotionList.add(new PromotionSecondHalf());
         ArrayList<Item> result = new ArrayList<Item>();
-        result.add(new Item(0, "ITEM000000", "可口可乐", "瓶", 3.00));
-        result.add(new Item(1, "ITEM000001", "雪碧", "瓶", 3.00));
-        result.add(new Item(2, "ITEM000002", "苹果", "瓶", 3.00));
-        result.add(new Item(3, "ITEM000003", "荔枝", "瓶", 3.00));
-        result.add(new Item(4, "ITEM000004", "电池", "瓶", 3.00));
-        result.add(new Item(5, "ITEM000005", "方便面", "瓶", 3.00));
+        result.add(new Item(0, "ITEM000000", "可口可乐", "瓶", 3.00, category, promotionList));
+        result.add(new Item(1, "ITEM000001", "雪碧", "瓶", 3.00, category, promotionList));
+        result.add(new Item(2, "ITEM000002", "苹果", "瓶", 3.00, category, promotionList));
+        result.add(new Item(3, "ITEM000003", "荔枝", "瓶", 3.00, category, promotionList));
+        result.add(new Item(4, "ITEM000004", "电池", "瓶", 3.00, category, promotionList));
+        result.add(new Item(5, "ITEM000005", "方便面", "瓶", 3.00, category, promotionList));
         return result;
     }
 
