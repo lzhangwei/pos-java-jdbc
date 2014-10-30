9ab2994e98ef52244df0a08d61055a8f0337a566
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 14:56:57 +0800

    add implements function

--- src/main/java/com/thoughtworks/iamcoach/dao/PromotionDaoImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/dao/PromotionDaoImpl.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.iamcoach.dao;
 
+import com.thoughtworks.iamcoach.vo.Promotion;
+
+import java.util.List;
+
 public class PromotionDaoImpl implements PromotionDao{
+    @Override
+    public Promotion getPromotionById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Promotion> getPromotions() {
+        return null;
+    }
 }
