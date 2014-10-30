e2b55ddfa26a8a9b948b616207505ff100c522c6
lzhangwei <zhangwei_999@foxmail.com>
2014-10-30 14:22:25 +0800

    update scanner test

--- src/test/java/com/thoughtworks/iamcoach/pos/ScannerTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/ScannerTest.java
@@ -9,11 +9,7 @@ import java.util.List;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class ScannerTest {
-    @Test
-    public void hello_world_test() {
-        assertThat("Hello World").isEqualTo("Hello World");
-    }
-
+    
     @Test
     public void scanner_test() {
         List<String> barcodes = new ArrayList<String>();
@@ -30,6 +26,7 @@ public class ScannerTest {
         Scanner scanner = new Scanner();
         List<String> result = scanner.readFile(path);
         assertThat(result.size()).isEqualTo(9);
+        assertThat(result.get(1)).isEqualTo("ITEM000001");
     }
 
 }
