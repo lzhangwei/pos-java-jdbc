4bf068ae467a0f896fd98434f58650679e2abdbc
lzhangwei <zhangwei_999@foxmail.com>
2014-10-31 20:46:11 +0800

    update getItems

--- src/main/java/com/thoughtworks/iamcoach/service/ItemServiceImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/service/ItemServiceImpl.java
@@ -1,18 +1,37 @@
 package com.thoughtworks.iamcoach.service;
 
+import com.thoughtworks.iamcoach.dao.CategoryDao;
+import com.thoughtworks.iamcoach.dao.PromotionDao;
+import com.thoughtworks.iamcoach.dao.impl.CategoryDaoImpl;
 import com.thoughtworks.iamcoach.dao.impl.ItemDaoImpl;
 import com.thoughtworks.iamcoach.dao.ItemDao;
+import com.thoughtworks.iamcoach.dao.impl.PromotionDaoImpl;
+import com.thoughtworks.iamcoach.vo.Category;
 import com.thoughtworks.iamcoach.vo.Item;
+import com.thoughtworks.iamcoach.vo.Promotion;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ItemServiceImpl implements ItemService {
 
     private ItemDao itemDao = new ItemDaoImpl();
+    private CategoryDao categoryDao = new CategoryDaoImpl();
+    private PromotionDao promotionDao = new PromotionDaoImpl();
 
     @Override
     public ArrayList<Item> getItems() {
-        return itemDao.getItems();
+        ArrayList<Item> items = itemDao.getItems();
+        for(Item item : items) {
+            Category category = categoryDao.getCategoryById(item.getCategory().getId());
+            item.setCategory(category);
+
+            List<Promotion> promotionList = promotionDao.getPromotionsByItemId(item.getId());
+            item.setPromotionList(promotionList);
+
+            item.setDiscount(promotionDao.getPromotionDiscount(item.getId()));
+        }
+        return items;
     }
 
     @Override
