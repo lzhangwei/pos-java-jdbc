8abb9387455cc9f7ba3f385365a389e2113b7d9b
lzhangwei <zhangwei_999@foxmail.com>
2014-10-31 19:35:39 +0800

    update service test

--- src/test/java/com/thoughtworks/iamcoach/service/ItemServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/service/ItemServiceTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.iamcoach.service;
 
+import com.thoughtworks.iamcoach.dao.ItemDao;
 import com.thoughtworks.iamcoach.dao.ItemDaoImpl;
 import com.thoughtworks.iamcoach.vo.*;
 import org.junit.Test;
@@ -14,33 +15,41 @@ import static org.mockito.Mockito.when;
 public class ItemServiceTest {
 
     private ArrayList<Item> createItems() {
-        Category category = new Category(1,"饮料");
+        Category category1 = new Category(1, "饮料");
+        Category category2 = new Category(2, "水果");
         List<Promotion> promotionList = new ArrayList<Promotion>();
         promotionList.add(new PromotionDiscount());
         promotionList.add(new PromotionSecondHalf());
         ArrayList<Item> result = new ArrayList<Item>();
-        result.add(new Item(0, "ITEM000000", "可口可乐", "瓶", 3.00, category, promotionList));
-        result.add(new Item(1, "ITEM000001", "雪碧", "瓶", 3.00, category, promotionList));
-        result.add(new Item(2, "ITEM000002", "苹果", "瓶", 3.00, category, promotionList));
-        result.add(new Item(3, "ITEM000003", "荔枝", "瓶", 3.00, category, promotionList));
-        result.add(new Item(4, "ITEM000004", "电池", "瓶", 3.00, category, promotionList));
-        result.add(new Item(5, "ITEM000005", "方便面", "瓶", 3.00, category, promotionList));
+        result.add(new Item(1, "ITEM000000", "可口可乐", "瓶", 3.00, category1, promotionList, 90));
+        result.add(new Item(2, "ITEM000001", "雪碧", "瓶", 3.00, category1, promotionList, 85));
+        result.add(new Item(3, "ITEM000002", "苹果", "瓶", 3.00, category2, promotionList, 95));
+        result.add(new Item(4, "ITEM000003", "荔枝", "瓶", 12.00, category2, promotionList, 80));
+        result.add(new Item(2, "ITEM000001", "雪碧", "瓶", 3.00, category1, promotionList, 85));
+        result.add(new Item(3, "ITEM000002", "苹果", "瓶", 8.00, category2, promotionList, 95));
         return result;
     }
 
     @Test
     public void get_item_by_barcode_test() {
         ItemDaoImpl itemDaoImpl = mock(ItemDaoImpl.class);
-        when(itemDaoImpl.getItemByBarcode("ITEM000001")).thenReturn(new Item(1, "ITEM000001", "雪碧", "瓶", 3.00));
+
+        List<Promotion> promotionList = new ArrayList<Promotion>();
+        promotionList.add(new PromotionSecondHalf());
+        Item item = new Item(2, "ITEM000001", "雪碧", "瓶", 3.00, new Category(1, "饮料"), promotionList, 100);
+
+        when(itemDaoImpl.getItemByBarcode("ITEM000001")).thenReturn(item);
         ItemService itemService = new ItemServiceImpl();
-        assertThat(itemService.getItemByBarcode("ITEM000001").getId()).isEqualTo(1);
+        assertThat(itemService.getItemByBarcode("ITEM000001").getId()).isEqualTo(2);
     }
 
     @Test
     public void get_items_test() {
         ItemDaoImpl itemDaoImpl = mock(ItemDaoImpl.class);
-        when(itemDaoImpl.getItems()).thenReturn(createItems());
+        ArrayList<Item> items = createItems();
+        when(itemDaoImpl.getItems()).thenReturn(items);
         ItemService itemService = new ItemServiceImpl();
         assertThat(itemService.getItems().size()).isEqualTo(6);
+        assertThat(itemService.getItems().get(3).getName()).isEqualTo("荔枝");
     }
 }
