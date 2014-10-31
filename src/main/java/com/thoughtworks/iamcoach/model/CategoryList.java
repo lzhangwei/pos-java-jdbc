34755eaa848964780df2f85c422a027f72ddea83
lzhangwei <zhangwei_999@foxmail.com>
2014-10-31 21:31:03 +0800

    add fields and function

--- src/main/java/com/thoughtworks/iamcoach/model/CategoryList.java
+++ src/main/java/com/thoughtworks/iamcoach/model/CategoryList.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.iamcoach.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryList {
+    private Category category;
+    private List<CartItem> cartItemList = new ArrayList<CartItem>();
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void addCartItem(CartItem cartItem) {
+        cartItemList.add(cartItem);
+    }
 }
